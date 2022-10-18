a38c1d42a26bd95edc030d210f45b38ee409f3d8
dhhyi <dhhyi@aol.com>
2022-10-18 15:09:19 +0000

    Update devcontainers to output generated at f18dd95

--- io/.update_devcontainer.sh
+++ io/.update_devcontainer.sh
@@ -2,4 +2,4 @@
 
 cd "$(dirname "$(readlink -f "$0")")"
 
-curl -so- https://raw.githubusercontent.com/dhhyi/devcontainer-creator/dist/bundle.js | node - dcc://io . --full --name "Example devcontainer for io"
+curl -so- https://raw.githubusercontent.com/dhhyi/devcontainer-creator/dist/bundle.js | node - dcc://io . --full --name "Example devcontainer for io" "$@"
