06e430a891147fb4943ae52dc99be9d01042bee7
dhhyi <dhhyi@aol.com>
2022-10-18 20:42:43 +0000

    Update devcontainers to output generated at 86443c3

--- erlang/.devcontainer/selftest.sh
+++ erlang/.devcontainer/selftest.sh
@@ -4,9 +4,10 @@ set -e
 set -x
 erl +V
 cat > /tmp/helloworld.erl <<EOF
--module(helloworld).
--export([main/1]).
-main(_) -> io:format("Hello World!\n").
+-module(helloworld). -export([main/1]).
+main(_) ->    io:format("Hello World!\n").
 EOF
-rebar3 format --files /tmp/helloworld.erl
-escript /tmp/helloworld.erl
+cd /tmp
+rebar3 fmt -w helloworld.erl
+cat helloworld.erl
+escript helloworld.erl
