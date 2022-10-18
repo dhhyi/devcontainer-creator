6fbb035c2f82b6e9564b3f2a133dae393eeb93a9
dhhyi <dhhyi@aol.com>
2022-10-18 09:12:55 +0000

    Update devcontainers to output generated at e6e2c10

--- lua/.devcontainer/disclaimer.sh
+++ lua/.devcontainer/disclaimer.sh
@@ -10,12 +10,12 @@ echo "Use the command 'lua' to start a REPL."
 echo "Use the command 'cont <script>' to continuously evaluate a given script."
 
 
-vscode=$(dirname $(dirname $0))/.vscode/tasks.json
+vscode="$1/.vscode/tasks.json"
 if [ -f "$vscode" ]
-echo
-echo "Alternatively you can use the VSCode tasks:"
 then
-    cat $vscode | egrep -v '^\s*\/\/.*' | jq -r ".tasks [] .label" | while read label; do
+    echo
+    echo "Alternatively you can use the VSCode tasks:"
+    cat "$vscode" | egrep -v '^\s*\/\/.*' | jq -r ".tasks [] .label" | while read label; do
         echo "- $label"
     done
 fi
