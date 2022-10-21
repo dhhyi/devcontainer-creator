94882d77559add3966f2269f06df1fe4e64ed860
dhhyi <dhhyi@aol.com>
2022-10-21 12:20:56 +0000

    Update devcontainers to output generated at a96d362

---
+++ go/.devcontainer/selftest.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+set -x
+go version
+cat >/tmp/helloworld.go <<EOF
+package main
+import "fmt"
+func main() { fmt.Println("Hello World!") }
+EOF
+go fmt /tmp/helloworld.go
+cat /tmp/helloworld.go
+go run /tmp/helloworld.go
