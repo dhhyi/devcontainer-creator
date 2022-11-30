a071395d3ac5e848dcffb3066eed612ad6a4551f
Danilo Hoffmann <dhhyi@aol.com>
2022-11-30 19:09:05 +0100

    validate languages in pre-commit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -11,4 +11,13 @@ if (cp.spawnSync('shellcheck', ['--version']).status === 0) {
 
 rules['features/**'] = [() => 'devcontainer features test features'];
 
+rules['examples/**'] = [
+  (files) => [
+    ...files.map(
+      (file) =>
+        `npm exec -- ajv validate -d ${file} -s templates/language_schema.json --errors=text --verbose`
+    ),
+  ],
+];
+
 module.exports = rules;
