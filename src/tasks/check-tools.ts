ea58961ad4d8cf641d4eea0c486f0b8a362136b0
Danilo Hoffmann <dhhyi@aol.com>
2023-10-05 14:46:34 +0200

    use correct semver compare

--- src/tasks/check-tools.ts
+++ src/tasks/check-tools.ts
@@ -5,15 +5,37 @@ import { once } from 'lodash-es';
 const INSTALL_LATEST_MESSAGE =
   'please install devcontainer-cli with `npm install -g @devcontainers/cli@latest`';
 
+function isSemVerLessThan(left: string, right: string) {
+  const partsLeft = left.split('.');
+  const partsRight = right.split('.');
+  for (let i = 0; i < 3; i++) {
+    const numLeft = Number(partsLeft[i]);
+    const numRight = Number(partsRight[i]);
+    if (numLeft > numRight) {
+      return false;
+    }
+    if (numRight > numLeft) {
+      return true;
+    }
+    if (!isNaN(numLeft) && isNaN(numRight)) {
+      return false;
+    }
+    if (isNaN(numLeft) && !isNaN(numRight)) {
+      return true;
+    }
+  }
+  return false;
+}
+
 export const DevcontainerCLIBin = once(() => {
   try {
     const version = execSync('devcontainer --version', {
       encoding: 'utf-8',
     }).trim();
-    const [maj, min, patch] = version.split('.');
-    // version must be later than 0.51.2
-    if (Number(maj) < 0 || Number(min) < 51 || Number(patch) < 2) {
-      throw new Error('minimum required devcontainer-cli version is 0.51.2');
+    if (isSemVerLessThan(version, '0.51.2')) {
+      throw new Error(
+        'minimum required devcontainer-cli version is 0.51.2, got ' + version
+      );
     }
   } catch (error) {
     console.log('\n');
