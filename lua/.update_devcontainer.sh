9a4691d603020c6bd55b94444f258378703480d9
dhhyi <dhhyi@aol.com>
2025-08-16 10:59:29 +0000

    Update devcontainers to output generated at 0536586

---
+++ lua/.update_devcontainer.sh
@@ -0,0 +1,5 @@
+#!/bin/sh -e
+
+cd "$(dirname "$(readlink -f "$0")")"
+
+curl -so- https://raw.githubusercontent.com/dhhyi/devcontainer-creator/dist/bundle.js | node - https://raw.githubusercontent.com/dhhyi/devcontainer-creator/main/examples/lua.yaml . --name "Example devcontainer for lua" "$@"
