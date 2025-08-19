29e5521c5c340dc9788ef0d8e7679725a05825db
Danilo Hoffmann <dhhyi@aol.com>
2025-08-19 13:02:37 +0200

    add filtering to test script

--- run-tests.sh
+++ run-tests.sh
@@ -5,20 +5,45 @@ if [ -z "$CI" ]; then
     npm exec pnpm run build
 fi
 
+if [ -z "$*" ]; then
+    set -- tests
+fi
+
+filter() {
+    if [ -z "$1" ]; then
+        echo "No test provided"
+        exit 0
+    fi
+
+    test="$1"
+    shift
+
+    for filter in "$@"; do
+        if expr $test : ".*$filter.*" > /dev/null 2>&1; then
+            return 0
+        fi
+    done
+    return 1
+}
+
 find tests -name 'test.dcc' | while read -r test; do
-    echo "Running $test"
-    node dist/bundle.js "$(head -n 1 "$test")" "$(dirname "$test")" --test --dump-meta
+    if filter "$test" "$@"; then
+        echo "Running $test"
+        node dist/bundle.js "$(head -n 1 "$test")" "$(dirname "$test")" --test --dump-meta
+    fi
 done
 
 find tests -name 'language.yaml' | while read -r test; do
-    echo "Running $test"
 
     args=""
     if [ -f "$(dirname "$test")/test-args.dcc" ]; then
         args="$(head -n 1 "$(dirname "$test")/test-args.dcc")"
     fi
-    # shellcheck disable=SC2086
-    node dist/bundle.js "$test" "$(dirname "$test")" --test --dump-meta $args
+    if filter "$test" "$@"; then
+        echo "Running $test"
+        # shellcheck disable=SC2086
+        node dist/bundle.js "$test" "$(dirname "$test")" --test --dump-meta $args
+    fi
 done
 
 if [ -n "$(git status -s)" ]; then
