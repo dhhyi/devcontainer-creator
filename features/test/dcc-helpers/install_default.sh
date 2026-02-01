7be7da2e90b63c2e79c581304f5263b458023cce
Danilo Hoffmann <dhhyi@aol.com>
2026-02-01 11:42:03 +0100

    install fish

--- features/test/dcc-helpers/install_default.sh
+++ features/test/dcc-helpers/install_default.sh
@@ -5,7 +5,13 @@ set -e
 # shellcheck disable=SC1091 # Import test library
 source dev-container-features-test-lib
 
-check "bash prompt" grep -q 'export PS1="$ "' "$HOME/.bashrc"
+user="$(cat /etc/passwd | grep "1000:" | cut -d: -f1)"
+
+check "fish default shell" grep -q "/home/$user:/usr/bin/fish" /etc/passwd
+
+check "fish prompt" grep -q 'fish_prompt' "/home/$user/.config/fish/config.fish"
+
+check "bash prompt" grep -q 'export PS1="$ "' "/home/$user/.bashrc"
 
 check "templates after install" test -f /disclaimer.sh && test -f /usr/local/bin/cont
 
