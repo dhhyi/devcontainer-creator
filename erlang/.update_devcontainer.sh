d81229882f0e7142a656d72e4ded3be33a6bbbcb
dhhyi <dhhyi@aol.com>
2026-09-07 09:53:43 +0000

    Update devcontainers to output generated at bf4d629

--- erlang/.update_devcontainer.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -e
-
-cd "$(dirname "$(readlink -f "$0")")"
-
-curl -so- https://raw.githubusercontent.com/dhhyi/devcontainer-creator/dist/bundle.js | node - https://raw.githubusercontent.com/dhhyi/devcontainer-creator/main/examples/erlang.yaml . --name "Example devcontainer for erlang" "$@"
